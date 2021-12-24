7179ebba472ef87fa525a198b4e40d785ecc8c40
Alex <okuzia@softserveinc.com>
2021-12-24 12:56:50 +0200

    Add project files

---
+++ Mango.Services.ProductApi/Program.cs
@@ -0,0 +1,39 @@
+using Mango.Services.ProductApi.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(GetConnectionString()));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+string GetConnectionString()
+{
+    var configurationRoot = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+    return configurationRoot.GetConnectionString(nameof(ApplicationDbContext));
+}
